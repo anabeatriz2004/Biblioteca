b3001dd0d6bd6e9ebb7d6f895a8193043cc25053
Ana Beatriz Vicente <58741499+anabeatriz2004@users.noreply.github.com>
2023-12-15 15:21:07 +0000

    corrigido emprestimo a partir do programa original

--- Biblioteca/src/main/java/org/example/Emprestimo.java
+++ Biblioteca/src/main/java/org/example/Emprestimo.java
@@ -22,11 +22,10 @@ public class Emprestimo {
         this.conexao = conexao;
     }
 
-    Emprestimo(Livro id_livro, Utilizador id_utilizador, Bibliotecario id_bibliotecario, Database conexao) {
+    Emprestimo(Livro id_livro, Utilizador id_utilizador, Bibliotecario id_bibliotecario) {
         this.livro = id_livro;
         this.utilizador = id_utilizador;
         this.bibliotecario = id_bibliotecario;
-        this.conexao = conexao; // Recebendo a conexão como parâmetro
     }
 
     public void devolverLivro() {
@@ -41,18 +40,16 @@ public class Emprestimo {
 
     private void atualizarNoBanco() {
         try {
-            conexao.conectar(); // Conectando ao banco usando a instância de ConexaoMySQL
-
             String sql = "UPDATE emprestimos SET data_devolvido = ? WHERE livro_id = ? AND utilizador_id = ?";
-            PreparedStatement preparedStatement = conexao.getConexao().prepareStatement(sql);
+
+            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
             preparedStatement.setTimestamp(1, java.sql.Timestamp.valueOf(dataDevolvido.atStartOfDay()));
-            preparedStatement.setInt(2, livro.getID());
-            preparedStatement.setInt(3, utilizador.getID());
+            preparedStatement.setInt(2, livro.getID_livro());
+            preparedStatement.setInt(3, utilizador.id_utilizador);
 
             preparedStatement.executeUpdate();
 
             preparedStatement.close();
-            conexao.desconectar(); // Desconectando após a operação
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -60,39 +57,40 @@ public class Emprestimo {
 
     public void emprestarLivro() {
         try {
-            conexao.conectar(); // Conectando ao banco usando a instância de ConexaoMySQL
             dataEmprestimo = LocalDate.now();
             dataDevolucao = LocalDate.now().plusDays(15);
+
             String sql = "INSERT INTO emprestimo (id_livro, id_utilizador, id_bibliotecario, data_emprestimo, data_devolucao) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement preparedStatement = conexao.getConexao().prepareStatement(sql);
-            preparedStatement.setInt(1, livro.getID());
-            preparedStatement.setInt(2, utilizador.getID());
-            preparedStatement.setInt(3, bibliotecario.getID());
+
+            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
+            preparedStatement.setInt(1, livro.getID_livro());
+            preparedStatement.setInt(2, utilizador.id_utilizador);
+            preparedStatement.setInt(3, bibliotecario.getId_bibliotecario());
             preparedStatement.setTimestamp(4, java.sql.Timestamp.valueOf(dataEmprestimo.atStartOfDay()));
             preparedStatement.setTimestamp(5, java.sql.Timestamp.valueOf(dataDevolucao.atStartOfDay()));
 
             preparedStatement.executeUpdate();
 
             preparedStatement.close();
-            conexao.desconectar(); // Desconectando após a operação
+
             atualizar_estado_livro(false); //Atualiza a disponibilidade do livro
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
     public void atualizar_estado_livro(boolean disponibilidade) {
         try {
-            conexao.conectar(); // Conectando ao banco usando a instância de Database
-
             String sql = "UPDATE livro SET disponibilidade = ? WHERE id_livro = ?";
-            PreparedStatement preparedStatement = conexao.getConexao().prepareStatement(sql);
+
+            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
             preparedStatement.setBoolean(1, disponibilidade);
-            preparedStatement.setInt(2, livro.getID());
+            preparedStatement.setInt(2, livro.getID_livro());
 
             preparedStatement.executeUpdate();
 
             preparedStatement.close();
-            conexao.desconectar(); // Desconectando após a operação
+
         } catch (SQLException e) {
             e.printStackTrace();
         }

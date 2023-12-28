91650f44e3ed2f6735c1bc2648135c941f9c1ea1
Ana Beatriz Vicente <58741499+anabeatriz2004@users.noreply.github.com>
2023-12-28 10:55:44 +0000

    removendo código desnecessário

---
+++ Biblioteca Desktop/src/Biblioteca.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Biblioteca extends JFrame {
+
+
+    public Biblioteca () {
+        // nome da aplicação
+        setTitle("Biblioteca");
+        // quando se feche a aplicção o programa termina
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //define tamanho da aplicação
+        setSize(300,200);
+        // para exibir no centro da tela
+        setLocationRelativeTo(null);
+        // para vizualizar
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Biblioteca();
+
+    }
+}

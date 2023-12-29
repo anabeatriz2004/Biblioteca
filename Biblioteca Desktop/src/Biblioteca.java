a507427df8f1f94699ef62955b528e34aa3cbfba
Ana Beatriz Vicente <58741499+anabeatriz2004@users.noreply.github.com>
2023-12-29 16:38:03 +0000

    tentando ver todos os livros na tabela

--- Biblioteca Desktop/src/Biblioteca.java
+++ Biblioteca Desktop/src/Biblioteca.java
@@ -1,13 +1,21 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Scanner;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Biblioteca extends JFrame {
+
+    private final Database conexao = new Database(); // Instanciar a classe Database
+
     private JLabel biblioteca;
     private JPanel PaginaInicialPanel;
     private JButton loginBotao;
+    private JTable tabela;
+    private DefaultTableModel table;
 
     Login l = new Login();
 
@@ -27,6 +35,28 @@ public class Biblioteca extends JFrame {
         // para vizualizar
         setVisible(true);
 
+        // Inicializa o modelo da tabela
+        tabela = new JTable();
+        tabela.addColumn("ID");
+        tabela.addColumn("ISBN");
+        tabela.addColumn("Título");
+        tabela.addColumn("Autor");
+        tabela.addColumn("Editora");
+        tabela.addColumn("Ano de Publicação");
+        tabela.addColumn("Gênero");
+        tabela.addColumn("Disponibilidade");
+
+        // Inicializa a tabela com o modelo
+        tabela = new JTable(table);
+
+        // Adiciona a tabela a um JScrollPane para permitir rolagem
+        JScrollPane scrollPane = new JScrollPane(table);
+        // Adiciona o JScrollPane ao JFrame
+        add(scrollPane);
+
+        // chama o método para vizualizar o tabela com os dados de todos os livros
+        consultarTodosLivros();
+
         // Botão Login
         loginBotao.addActionListener(new ActionListener() {
             @Override
@@ -38,7 +68,58 @@ public class Biblioteca extends JFrame {
         });
     }
 
+    // Método para consultar todos os livros na base de dados
+    public void consultarTodosLivros() {
+        try {
+            // Define a consulta SQL para selecionar todos os livros
+            String sql = "SELECT * FROM livro";
+
+            // Cria o objeto PreparedStatement
+            try (PreparedStatement pstmt = conexao.getConexao().prepareStatement(sql)) {
+                // Executa a consulta
+                ResultSet resultado = pstmt.executeQuery();
+
+                // Processa os resultados
+                while (resultado.next()) {
+                    // Recupera os dados do livro
+                    long idLivro = resultado.getLong("id_livro");
+                    String isbn = resultado.getString("ISBN");
+                    String titulo = resultado.getString("titulo");
+                    String autor = resultado.getString("autor");
+                    String editora = resultado.getString("editora");
+                    int anoPubli = resultado.getInt("ano_publi");
+                    String genero = resultado.getString("genero");
+                    boolean disponibilidade = resultado.getBoolean("disponibilidade");
+
+                    // Faça o que deseja com os dados, por exemplo, imprimir na tela
+                    System.out.println("ID: " + idLivro + ", ISBN: " + isbn + ", Título: " + titulo +
+                            ", Autor: " + autor + ", Editora: " + editora + ", Ano de Publicação: " + anoPubli +
+                            ", Gênero: " + genero + ", Disponibilidade: " + disponibilidade);
+                }
+
+                while (resultado.next()) {
+                    // Adiciona uma nova linha à tabela para cada livro encontrado
+                    tabela.addRow(new Object[]{
+                            resultado.getLong("id_livro"),
+                            resultado.getString("ISBN"),
+                            resultado.getString("titulo"),
+                            resultado.getString("autor"),
+                            resultado.getString("editora"),
+                            resultado.getInt("ano_publi"),
+                            resultado.getString("genero"),
+                            resultado.getBoolean("disponibilidade")
+                    });
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
+
+        // inicia a aplicação
         new Biblioteca();
     }
 }

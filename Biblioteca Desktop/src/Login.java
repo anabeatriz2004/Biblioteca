097b4cc778da4b6138883ab7e26945c3dfae9d88
Ana Beatriz Vicente <58741499+anabeatriz2004@users.noreply.github.com>
2023-12-29 10:15:01 +0000

    acrescenta verificar login

--- Biblioteca Desktop/src/Login.java
+++ Biblioteca Desktop/src/Login.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Login extends JFrame {
     private JPanel LoginPanel;
@@ -10,6 +12,13 @@ public class Login extends JFrame {
     private JTextField EmailTextField;
     private JPasswordField PasswordField;
     private JButton LogarBotao;
+
+    // private Database conexao;
+    private Database conexao = new Database(); // Instanciar a classe Database
+
+    Menu menu = new Menu();
+
+    boolean entradaValida = false;
 
     public Login() {
         // nome da aplicação
@@ -24,5 +33,70 @@ public class Login extends JFrame {
         setLocationRelativeTo(null);
         // para vizualizar
         setVisible(true);
+    }
+
+    public void verificarLogin(String email, String password) {
+        try {
+            // Define a consulta SQL para selecionar um utilizador com base no email
+            String usuarioSql = "SELECT * FROM utilizador WHERE email = ?";
+            // Define a consulta SQL para selecionar um bibliotecario com base no email
+            String bibliotecarioSql = "SELECT * FROM bibliotecario WHERE email = ?";
+
+            // Cria os objetos PreparedStatement para utilizador e bibliotecario
+            try (PreparedStatement pstmtUsuario = conexao.getConexao().prepareStatement(usuarioSql);
+                 PreparedStatement pstmtBibliotecario = conexao.getConexao().prepareStatement(bibliotecarioSql)) {
+
+                // Define o parâmetro da consulta com base no email fornecido
+                pstmtUsuario.setString(1, email);
+                pstmtBibliotecario.setString(1, email);
+
+                // Executa as consultas
+                ResultSet resultSetUsuario = pstmtUsuario.executeQuery();
+                ResultSet resultSetBibliotecario = pstmtBibliotecario.executeQuery();
+
+                // Verifica se o email foi encontrado na tabela de utilizador
+                if (resultSetUsuario.next()) {
+                    // Email encontrado, verifica a senha
+                    String senhaUsuario = resultSetUsuario.getString("senha");
+                    if (senhaUsuario.equals(password)) {
+                        // Senha correta, login bem-sucedido como utilizador
+                        System.out.println("Entrou como utilizador");
+                        menu.percorrerMenuUtilizador();
+                        entradaValida = true;
+                        return;
+                    } else {
+                        // Senha incorreta para utilizador
+                        entradaValida = false;
+                        System.out.println("Senha incorreta para utilizador, por favor, tente novamente");
+                        return;
+                    }
+                }
+
+                // Verifica se o email foi encontrado na tabela de bibliotecario
+                if (resultSetBibliotecario.next()) {
+                    // Email encontrado, verifica a senha
+                    String senhaBibliotecario = resultSetBibliotecario.getString("senha");
+                    if (senhaBibliotecario.equals(password)) {
+                        // Senha correta, login bem-sucedido como bibliotecario
+                        System.out.println("Entrou como bibliotecario");
+                        menu.percorrerMenuBibliotecario();
+                        entradaValida = true;
+                        return;
+                    } else {
+                        // Senha incorreta para bibliotecario
+                        entradaValida = false;
+                        System.out.println("Senha incorreta para bibliotecario, por favor, tente novamente");
+                        return;
+                    }
+                }
+
+                // Email não encontrado em ambas as tabelas
+                entradaValida = false;
+                System.out.println("Utilizador ou bibliotecario não encontrado");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao conectar ao banco de dados");
+        }
     }
 }

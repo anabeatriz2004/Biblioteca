aedbd93fc525bdd6ec47aaac3aa5952c33027256
Ana Beatriz Vicente <58741499+anabeatriz2004@users.noreply.github.com>
2023-12-30 17:45:51 +0000

    mostrando os dados mas agora só apartir do Tabela

--- Biblioteca Desktop/src/Tabela.java
+++ Biblioteca Desktop/src/Tabela.java
@@ -1,23 +1,47 @@
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Tabela extends JFrame {
 
     Livro l = new Livro();
     private JTable tabela;
-    private DefaultTableModel model;
+    private JPanel panel1;
 
     Tabela() {
-        // Defina o título
-        super("Tabela Demo");
+        // vai buscar o painel Principal
+        setContentPane(panel1);
+        // nome da aplicação
+        setTitle("Tabela");
+        // quando se feche a aplicção o programa termina
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // Obtém a resolução da tela
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        //define tamanho da aplicação
+        setSize(screenSize);
+        // para exibir no centro da tela
+        setLocationRelativeTo(null);
+        // para vizualizar
+        setVisible(true);
+
+        // nome das colunas
+        String[] colunas = {"id_livro", "ISBN", "titulo", "autor", "editora", "ano de publicação", "gênero", "disponibilidade"};
+
+        // Carregue os dados na tabela
+        String[][] linhas = loadData();
 
         // Crie o modelo da tabela
-        model = new DefaultTableModel();
-        model.setColumnIdentifiers(new String[]{"id_livro", "ISBN", "titulo", "autor", "editora", "ano de publicação", "gênero", "disponibilidade"});
+        DefaultTableModel tabelaModelo = (new DefaultTableModel(linhas, colunas) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        });
 
-        // Crie a tabela com o modelo
-        tabela = new JTable(model);
+        // diz que a JTable é baseada DefaultTableModel
+        tabela.setModel(tabelaModelo);
 
         // Adicione a tabela a um JScrollPane
         JScrollPane scrollPane = new JScrollPane(tabela);
@@ -25,49 +49,47 @@ public class Tabela extends JFrame {
         // Adicione o JScrollPane à frame
         add(scrollPane);
 
-        // Defina o modo de fechamento
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        // Defina o tamanho certo da frame
-        pack();
-
         // Para exibir no centro da tela
         setLocationRelativeTo(null);
-
-        // Carregue os dados na tabela
-        loadData();
 
         // Para visualizar
         setVisible(true);
     }
 
     // Método para carregar todos os métodos na tabela
-    private void loadData() {
+    private String[][] loadData() {
         try {
             // Armazene os dados em um ArrayList
-            ArrayList<Livro> todosOsLivros;
+            ArrayList<Livro> todosOsLivros = l.consultarTodosLivros();
 
-            // Vai buscar os dados de todos os livros
-            todosOsLivros = l.consultarTodosLivros();
+            // Crie uma matriz para armazenar os dados
+            String[][] dados = new String[todosOsLivros.size()][8]; // Assumindo que existem 8 colunas
 
-            // Adicione os dados ao modelo da tabela
-            for (Livro livro : todosOsLivros) {
-                model.addRow(new Object[]{
-                        livro.getID_livro(),
+            // Adicione os dados à matriz
+            for (int i = 0; i < todosOsLivros.size(); i++) {
+                Livro livro = todosOsLivros.get(i);
+                dados[i] = new String[]{
+                        String.valueOf(livro.getID_livro()),
                         livro.getISBN(),
                         livro.getTitulo(),
                         livro.getAutor(),
                         livro.getEditora(),
-                        livro.getAnoPubli(),
+                        String.valueOf(livro.getAnoPubli()),
                         livro.getGenero(),
-                        livro.isDisponibilidade()
-                });
+                        String.valueOf(livro.isDisponibilidade())
+                };
             }
 
+            // Retorne a matriz de dados
+            return dados;
+
         } catch (Exception e) {
+            // Trate a exceção conforme necessário
             e.printStackTrace();
+            return null; // Ou lançar uma exceção, dependendo do seu requisito
         }
     }
+
 
 
     public static void main(String[] args) {

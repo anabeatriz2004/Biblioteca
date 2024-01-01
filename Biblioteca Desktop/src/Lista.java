ef8130525ce25ffe872c5ac1f3f1f42f7bcadf9a
Ana Beatriz Vicente <58741499+anabeatriz2004@users.noreply.github.com>
2024-01-01 23:03:44 +0000

    Adicionado uma lista para cada tipo de utilizadores e documentar o que cada um faz

--- Biblioteca Desktop/src/Lista.java
+++ Biblioteca Desktop/src/Lista.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Lista {
 
-    Livro l = new Livro();
+    Livro livro = new Livro();
 
     JFrame frame = new JFrame("Biblioteca");
     JList<Livro> lista = new JList<>();
@@ -16,10 +16,9 @@ public class Lista {
     JButton alterarDadosButton = new JButton("Alterar Dados");
     JButton adicionarLivroButton = new JButton("Adicionar Livro");
 
-    public Lista() {
-        lista.setModel(listaModelo);
+    public Lista() {}
 
-        ArrayList<Livro> todosOsLivros = l.consultarTodosLivros();
+    /** Método para mostrar a lista que é exibida ao arrancar o programa */
         for (int i = 0; i < todosOsLivros.size(); i++) {
             Livro livro = todosOsLivros.get(i);
             listaModelo.addElement(livro);
@@ -52,6 +51,89 @@ public class Lista {
         frame.setVisible(true);
     }
 
+    /** Método para mostrar a lista que é exibida, se caso for utilizador
+     * pode visualizar todos os livros, pode 'emprestar' e devolver livros */
+    public void ListaUtilizador() {
+        lista.setModel(listaModelo);
+
+        ArrayList<Livro> todosOsLivros = livro.consultarTodosLivros();
+        for (int i = 0; i < todosOsLivros.size(); i++) {
+            Livro livro = todosOsLivros.get(i);
+            listaModelo.addElement(livro);
+        }
+
+        lista.setCellRenderer(new LivroRenderer());
+
+        lista.getSelectionModel().addListSelectionListener(e -> {
+            Livro livro = lista.getSelectedValue();
+            exibirDetalhesLivro(livro);
+        });
+
+        // Adiciona um ouvinte de ação para o botão "Alterar Dados"
+        alterarDadosButton.addActionListener(e -> alterarDadosLivro());
+
+        // Adiciona um ouvinte de ação para o botão "Adicionar Livro"
+        adicionarLivroButton.addActionListener(e -> adicionarLivro());
+
+        JPanel leftPanel = new JPanel(new BorderLayout());
+        leftPanel.add(adicionarLivroButton, BorderLayout.NORTH);
+        leftPanel.add(new JScrollPane(lista), BorderLayout.CENTER);
+
+        splitPane.setLeftComponent(leftPanel);
+        splitPane.setRightComponent(painel);
+
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.add(splitPane);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    /** Método para mostrar a lista que é exibida, se caso for bibliotecario,
+     * este pode visualizar todos os livros, tal como o utilizador,
+     * pode adicionar livro, alterar livro, e eliminar um livro, também têm acesso há
+     * tabela emprestimo, para visualizar os mesmos*/
+    public void ListaBibliotecario() {
+        lista.setModel(listaModelo);
+
+        ArrayList<Livro> todosOsLivros = livro.consultarTodosLivros();
+        for (int i = 0; i < todosOsLivros.size(); i++) {
+            Livro livro = todosOsLivros.get(i);
+            listaModelo.addElement(livro);
+        }
+
+        lista.setCellRenderer(new LivroRenderer());
+
+        lista.getSelectionModel().addListSelectionListener(e -> {
+            Livro livro = lista.getSelectedValue();
+            exibirDetalhesLivro(livro);
+        });
+
+        // Adiciona um ouvinte de ação para o botão "Alterar Dados"
+        // NÃO FUNCIONA
+        alterarDadosButton.addActionListener(e -> alterarDadosLivro());
+
+        // Adiciona um ouvinte de ação para o botão "Adicionar Livro"
+        adicionarLivroButton.addActionListener(e -> adicionarLivro());
+        // NÃO FUNCIONA
+
+        JPanel leftPanel = new JPanel(new BorderLayout());
+        leftPanel.add(adicionarLivroButton, BorderLayout.NORTH);
+        leftPanel.add(new JScrollPane(lista), BorderLayout.CENTER);
+
+        splitPane.setLeftComponent(leftPanel);
+        splitPane.setRightComponent(painel);
+
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.add(splitPane);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    /** Método para mostrar a mostrar todos os dethalhes na lista,
+     * pode ser usado or três mêtodos:
+     * ListaGenerica(), ListaUtilizador(), ListaBibliotecario() */
     private void exibirDetalhesLivro(Livro livro) {
         textArea.setText("ISBN: " + livro.getISBN() +
                 "\nTitulo: " + livro.getTitulo() +
